2ec3173b340102602111814d64f37466aa7cb2c0
Hector Cruz <hcruz@solserin.com>
2020-03-18 11:38:43 -0700

    seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -5,28 +5,32 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        $this->call(SeccionesSeeder::class);
-        $this->call(ModulosSeeder::class);
-        $this->call(RolesSeederTable::class);
-        $this->call(EstadosTableSeeder::class);
-        $this->call(MunicipiosTableSeeder::class);
-        $this->call(SATMonedasSeeder::class);
-        $this->call(SATRegimenesSeeder::class);
-        $this->call(tiposPropiedadesSeeder::class);
-        $this->call(PropiedadesSeeder::class);
-        $this->call(columnasFilasSeeder::class);
-        $this->call(SATProductosServiciosSeeder::class);
-        $this->call(TiposProductoSeeder::class);
-        $this->call(UnidadesSeeder::class);
-        $this->call(AlmacenesSeeder::class);
-        $this->call(FamiliasTableSeeder::class);
-        factory(User::class, 250)->create();
-    }
+        /**
+         * Seed the application's database.
+         *
+         * @return void
+         */
+        public function run()
+        {
+                $this->call(SeccionesSeeder::class);
+                $this->call(ModulosSeeder::class);
+                $this->call(RolesSeederTable::class);
+                $this->call(EstadosTableSeeder::class);
+                $this->call(MunicipiosTableSeeder::class);
+                $this->call(SATMonedasSeeder::class);
+                $this->call(SATRegimenesSeeder::class);
+                $this->call(tiposPropiedadesSeeder::class);
+                $this->call(PropiedadesSeeder::class);
+                $this->call(columnasFilasSeeder::class);
+                $this->call(SATProductosServiciosSeeder::class);
+                $this->call(TiposProductoSeeder::class);
+                $this->call(UnidadesSeeder::class);
+                $this->call(AlmacenesSeeder::class);
+                $this->call(FamiliasTableSeeder::class);
+                $this->call(formasPagoSeeder::class);
+                $this->call(tiposVentaSeeder::class);
+                $this->call(ventasReferenciasSeeder::class);
+                $this->call(antiguedadVentasSeeder::class);
+                factory(User::class, 250)->create();
+        }
 }
