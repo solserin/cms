48db8df5f0896606786dfaa8ffcf3a78414dc15a
Hector Cruz <hcruz@solserin.com>
2020-06-21 23:48:57 -0600

    f

--- resources/js/src/views/pages/cementerio/ventas/VentasList.vue
+++ resources/js/src/views/pages/cementerio/ventas/VentasList.vue
@@ -19,7 +19,7 @@
         >
           <img
             class="cursor-pointer img-btn"
-            src="../../../../../../../public/images/app/lista_precios.svg"
+            src="@assets/images/lista_precios.svg"
           />
           <span class="texto-btn">Planes de Venta</span>
         </vs-button>
