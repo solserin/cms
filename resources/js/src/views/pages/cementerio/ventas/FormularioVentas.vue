897a0ebc470ad2300c6b5141ec613fa4a916fa79
Hector Cruz <hcruz@solserin.com>
2020-06-21 23:52:05 -0600

    f

--- resources/js/src/views/pages/cementerio/ventas/FormularioVentas.vue
+++ resources/js/src/views/pages/cementerio/ventas/FormularioVentas.vue
@@ -2012,6 +2012,7 @@ export default {
       this.$vs.loading();
       try {
         let res = await cementerio.guardar_venta(this.form);
+        console.log("guardar_venta -> res", res);
         if (res.data >= 1) {
           //success
           this.$vs.notify({
@@ -2038,6 +2039,7 @@ export default {
         this.$vs.loading.close();
       } catch (err) {
         if (err.response) {
+          console.log("guardar_venta -> err.response", err.response);
           if (err.response.status == 403) {
             /**FORBIDDEN ERROR */
             this.$vs.notify({
