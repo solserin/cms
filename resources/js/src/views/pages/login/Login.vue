01fb2f7d1cd1362cc37ed4683fc651fbe9118766
hectorcrzprz@gmail.com <hectorcrzprz@gmail.com>
2021-03-25 11:43:21 -0700

    ux fix login

--- resources/js/src/views/pages/login/Login.vue
+++ resources/js/src/views/pages/login/Login.vue
@@ -1,16 +1,16 @@
 <template>
   <div
-    class="h-screen flex w-full bg-img vx-row no-gutter items-center justify-center"
+    class="h-screen flex w-full vx-row no-gutter items-center justify-center"
     id="page-login"
   >
-    <div class="layer"></div>
-    <div class="vx-col sm:w-4/5 md:w-1/2 lg:w-3/4 xl:w-3/5 sm:m-0 m-4">
-      <vx-card class="login-form">
+    <div class="layer z-index-1"></div>
+    <div class="vx-col sm:w-4/5 md:w-1/2 lg:w-3/4 xl:w-3/5 sm:m-0">
+      <vx-card class="login-form py-32">
         <div slot="no-body" class>
           <div class="vx-row no-gutter justify-center items-center">
             <div class="vx-col hidden lg:block lg:w-1/2">
               <img
-                src="@assets/images/logo/aeternus-siiga.png"
+                src="@assets/images/sistemas_aeternus.svg"
                 alt="login"
                 class="mx-auto login-img"
               />
@@ -18,15 +18,15 @@
                 <div
                   class="w-full sm:w-12/12 md:w-11/12 lg:w-11/12 xl:w-11/12 mr-auto ml-auto"
                 >
-                  <h1 class="text-textos_theme sistema-title uppercase">
+                  <h1
+                    class="uppercase color-dark-800 font-medium h3 text-center pt-6"
+                  >
                     SIIGA | Aeternus Funerales
                   </h1>
-                  <h2
-                    class="mt-3 text-lg text-textos_theme font-light text-center sistema-descripcion"
-                  >
+                  <h2 class="text-center size-base font-normal py-2">
                     Sistema Integral de Información y Gerencia Administrativa
                   </h2>
-                  <p class="mt-3 text-sm text-textos_theme text-center">
+                  <p class="text-center size-small font-light py-2">
                     ©2020 Todos los Derechos Reservados
                   </p>
                 </div>
@@ -34,15 +34,17 @@
             </div>
 
             <div class="vx-col sm:w-full md:w-full lg:w-1/2">
-              <div class="px-8 pt-8">
+              <div class="px-8">
                 <div class="flex flex-wrap">
                   <div
                     class="w-full sm:w-12/12 md:w-12/12 lg:w-12/12 xl:w-12/12 mr-auto ml-auto px-2"
                   >
-                    <div class="justify-center items-center md:mt-40">
-                      <h4 class="iniciar-sesion my-5">Inicia sesión</h4>
-                      <div class="w-full my-2">
-                        <label class="text-sm opacity-75"
+                    <div class="justify-center items-center">
+                      <h4 class="color-dark-800 h1 font-medium py-4">
+                        Inicia sesión
+                      </h4>
+                      <div class="w-full py-2">
+                        <label class="size-small color-copy"
                           >Correo electrónico</label
                         >
                         <vs-input
@@ -61,8 +63,8 @@
                           errors.first("email")
                         }}</span>
                       </div>
-                      <div class="w-full">
-                        <label class="text-sm opacity-75">Contraseña</label>
+                      <div class="w-full py-2">
+                        <label class="size-small color-copy">Contraseña</label>
                         <vs-input
                           v-validate="'required|min:6|max:25'"
                           type="password"
@@ -82,7 +84,7 @@
                       </div>
 
                       <div
-                        class="flex flex-wrap justify-between my-5 text-sm text-black"
+                        class="flex flex-wrap justify-between size-small smaller-line py-2"
                       >
                         Esta página está protegida y sujeta a las Políticas de
                         privacidad y a las Condiciones de la empresa. Acceso
@@ -99,14 +101,14 @@
                         >
                       </div>
 
-                      <div class="flex flex-wrap justify-between pt-8">
+                      <div class="flex flex-wrap justify-between">
                         <router-link
                           to="/pages/forgot-password"
                           class="text-textos_theme"
                           >¿Olvidaste tu contraseña?</router-link
                         >
                       </div>
-                      <div class="flex flex-wrap justify-between py-10">
+                      <div class="flex flex-wrap justify-between">
                         <a
                           href="#"
                           target="_blank"
