249b5c02e84af467dc9af1c3f1f7d6ee0b855074
hectorcrzprz@gmail.com <hectorcrzprz@gmail.com>
2021-04-06 10:28:23 -0600

    iniciando en planes funerarios

--- resources/js/themeConfig.js
+++ resources/js/themeConfig.js
@@ -26,13 +26,13 @@ let colors = {
 
 // CONFIGS
 const themeConfig = {
-    disableCustomizer: false, // options[Boolean] : true, false(default)
-    disableThemeTour: false, // options[Boolean] : true, false(default)
+    disableCustomizer: true, // options[Boolean] : true, false(default)
+    disableThemeTour: true, // options[Boolean] : true, false(default)
     footerType: "static", // options[String]  : static(default) / sticky / hidden
-    hideScrollToTop: false, // options[Boolean] : true, false(default)
-    mainLayoutType: "horizontal", // options[String]  : vertical(default) / horizontal
+    hideScrollToTop: true, // options[Boolean] : true, false(default)
+    mainLayoutType: "vertical", // options[String]  : vertical(default) / horizontal
     navbarColor: "#fff", // options[String]  : HEX color / rgb / rgba / Valid HTML Color name - (default: #fff)
-    navbarType: "static", // options[String]  : floating(default) / static / sticky / hidden
+    navbarType: "floating", // options[String]  : floating(default) / static / sticky / hidden
     routerTransition: "fade-bottom", // options[String]  : zoom-fade / slide-fade / fade-bottom / fade / zoom-out / none(default)
     rtl: false, // options[Boolean] : true, false(default)
     sidebarCollapsed: false, // options[Boolean] : true, false(default)
