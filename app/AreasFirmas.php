ec48f8df22efa82a8b60c78dd3822c180854eebc
hectorcrzprz@gmail.com <hectorcrzprz@gmail.com>
2021-04-16 13:03:01 -0600

    modelos para firmas listos

---
+++ app/AreasFirmas.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AreasFirmas extends Model
+{
+    protected $table = 'areas_firmas';
+}
---
+++ app/Firmas.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Firmas extends Model
+{
+     protected $table = 'firmas';
+}
